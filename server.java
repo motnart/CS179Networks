d3632f0a1fc06008e4d3193a69d47d8b594a5ac0
jzhan009 <jzhan009@gmail.com>
2014-05-08 18:47:46 -0700

    Update server.java

--- server.java
+++ server.java
@@ -1,4 +1,26 @@
-CS179Networks
-=============
+import java.io.*;
+import java.net.*;
 
-CS 179 Network
+class TCPServer
+{
+   public static void main(String argv[]) throws Exception
+      {
+         String clientSentence;
+         String capitalizedSentence;
+         ServerSocket welcomeSocket = new ServerSocket(6789);
+System.out.println("testing ");
+
+         while(true)
+         {
+System.out.println("listening");
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient =
+               new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+         }
+      }
+}
